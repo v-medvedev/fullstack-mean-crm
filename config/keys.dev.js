75cdc9de3f4676cc6c85575a25b8b45c8f0d9a08
VITAL-PC\VITAL <vmiadzvedzeu@gmail.com>
2019-05-18 13:21:47 +0300

    config updates

--- config/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://fullstack_admin:lL2Un7t4dmJaTlhE@fullstack-mean-shard-00-00-gn3we.mongodb.net:27017,fullstack-mean-shard-00-01-gn3we.mongodb.net:27017,fullstack-mean-shard-00-02-gn3we.mongodb.net:27017/test?ssl=true&replicaSet=fullstack-mean-shard-0&authSource=admin&retryWrites=true',
-    jwtSecret: 'dev_jwt'
-};
