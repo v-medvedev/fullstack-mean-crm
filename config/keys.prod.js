f456f00d6ddb7b4a16b9ed4992e0603e2bb017cd
VITAL-PC\VITAL <vmiadzvedzeu@gmail.com>
2019-05-18 13:19:15 +0300

    keys

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,5 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./keys.prod');
-} else {
-    module.exports = require('./keys.dev');
-}
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET
+};
